97cd8301dedbe0d2f40729bec799e1c1473e94df
ddoniddoni <psdkei@naver.com>
2022-11-07 22:20:57 +0900

    learn basic redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
+import { legacy_createStore } from "redux";
+
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
 const number = document.querySelector(".number");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const conutModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-const handlePlus = () => {
-  count = count + 1;
-  updateText();
+const countStore = legacy_createStore(conutModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
 };
 
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({ type: "MINUS" });
 };
 
-plus.addEventListener("click", handlePlus);
+plus.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);

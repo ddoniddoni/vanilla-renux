840bd4c1a5dec42525ef24478c1bdc7f0c81ffba
unknown <psdkei@naver.com>
2023-03-22 00:01:54 +0900

    update redux

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const DELETE_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text }];
+      return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
       return [];
     default:
